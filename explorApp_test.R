d32c07f4db1f5fe6cfa760c406c9a7d43ec0cdb3
Oliver Rojas <oliver.rojas.b@gmail.com>
2021-06-08 23:14:07 -0400

    new func

--- explorApp_test.R
+++ explorApp_test.R
@@ -20,3 +20,4 @@ datos <- datos[['datos']]
 
 
 
+
--- gitLoader.R
+++ gitLoader.R
@@ -20,16 +20,16 @@ explorAppLoader <- function(){
     
     # ---- Cargar funciones -------------------------------------------
     
-    # setwd('D:/GoogleDrive/R/ExplorApp/funciones')
-    # funcs <- dir()
-    # funcs <- funcs[grep("[^rR]", funcs)]
-    # funcs <- paste0("'", funcs, "'")
-    # funcs <- paste(funcs, collapse = ', ')
-    # funcs <- paste('funcs <- c(', funcs, ')')
-    # writeClipboard(funcs)
+    setwd('D:/GoogleDrive/R/ExplorApp/funciones')
+    funcs <- dir()
+    funcs <- funcs[grep("[^rR]", funcs)]
+    funcs <- paste0("'", funcs, "'")
+    funcs <- paste(funcs, collapse = ', ')
+    funcs <- paste('funcs <- c(', funcs, ')')
+    writeClipboard(funcs)
     
     # Cargar fnciones
-    funcs <- c( 'data01_dataLoader.R', 'data01_LeerExcel.R', 'data01_LeerStata.R', 'data02_DetectFactor.R', 'data02_DetectMissing.R', 'data02_DetectNumeric.R', 'data02_DetectOutlier.R' )
+    funcs <- c( 'data01_dataLoader.R', 'data01_LeerExcel.R', 'data01_LeerStata.R', 'data02_DetectClass.R', 'data02_DetectFactor.R', 'data02_DetectMissing.R', 'data02_DetectNumeric.R', 'data02_DetectOutlier.R' )
     url <- 'https://raw.githubusercontent.com/olitroski/ExplorApp/main/funciones/'
     for (f in funcs){
         print(paste('Cargando: ', f))
